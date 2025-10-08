b9b85a01c400c4b8257b7bb584ea0b301c8d3d7e
wisalMomand <mwisal80045@gmail.com>
2025-10-08 00:02:44 -0700

    Move backend to root for Railway deployment

--- Admin_Bakend/routes/adminRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { listUsers, updateUserStatus } = require("../controllers/adminController");
-
-const router = express.Router();
-
-router.get("/users", listUsers);
-router.patch("/users/:id/status", updateUserStatus);
-
-module.exports = router;
